47c9d5bc66cfca331b658593299ae9b295d8c05d
Jack Downie <downiej3@cardiff.ac.uk>
2026-03-24 17:03:50 +0000

    Improved dashboard functionality

--- app/components/createChart.js
+++ app/components/createChart.js
@@ -1,22 +1,16 @@
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Chart } from "react-chartjs-2";
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(...registerables);
 
-export default function Output_Chart({ data }) {
+export default function Output_Chart({ data, chartType }) {
   const chartData = {
     labels: data.map((d) => d.date),
     datasets: [
       {
         label: "Actual_income",
         data: data.map((item) => item.actual_income),
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        backgroundColor: "#bc509174",
         borderColor: "#bc5090",
         borderWidth: 1,
         tension: 0.1,
@@ -24,7 +18,7 @@ export default function Output_Chart({ data }) {
       {
         label: "Actual_total_spend",
         data: data.map((item) => item.actual_total_spend),
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        backgroundColor: "#003f5c6d",
         borderColor: "#003f5c",
         borderWidth: 1,
         tension: 0.1,
@@ -32,7 +26,7 @@ export default function Output_Chart({ data }) {
       {
         label: "Actual_net_cashflow",
         data: data.map((item) => item.actual_net_cashflow),
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        backgroundColor: "#ffa6007c",
         borderColor: "#ffa600",
         borderWidth: 1,
         tension: 0.1,
@@ -40,5 +34,5 @@ export default function Output_Chart({ data }) {
     ],
   };
 
-  return <Line data={chartData} />;
+  return <Chart type={chartType} data={chartData} />;
 }
